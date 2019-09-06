e152f57caf8f89dd31d5cf34d94c99187513af11
Fabian Große <saphareas@googlemail.com>
2019-09-06 11:57:42 +0200

    Publish EU vote results

---
+++ js/EU19_TabelleTerritorialkreis.js
@@ -0,0 +1,180 @@
+var data0 =["Gesamt",
+["Wahlberechtigte","2048602","X","X","2060156","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","1218415","59.5","12.8","961109","46.7"],
+["darunter Briefwähler","265353","21.8","3.9","171643","17.9"],
+["Ungültige Stimmen","20985","1.7","-1.0","26563","2.8"],
+["Gültige Stimmen","1197430","98.3","1.0","934546","97.2"],
+["DKP","2381","0.2","-0.1","2435","0.3"]];
+
+var data1 = ["Brandenburg Havel",
+["Wahlberechtigte","58801","X","X","60560","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","28677","48.8","12.3","22078","36.5"],
+["darunter Briefwähler","4472","15.6","1.4","3126","14.2"],
+["Ungültige Stimmen","501","1.7","-1.2","649","2.9"],
+["Gültige Stimmen","28176","98.3","1.2","21429","97.1"],
+["DKP","64","0.2","-0.1","69","0.3"]];
+
+var data2 = ["Cottbus",
+["Wahlberechtigte","78559","X","X","82908","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","45065","57.4","17.2","33305","40.2"],
+["darunter Briefwähler","11177","24.8","1.7","7693","23.1"],
+["Ungültige Stimmen","782","1.7","-0.5","732","2.2"],
+["Gültige Stimmen","44283","98.3","0.5","32573","97.8"],
+["DKP","118","0.3","-0.1","108","0.3"]];
+
+var data3 =["Frankfurt (Oder)",
+["Wahlberechtigte","44982","X","X","47810","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","22987","51.1","9.6","19836","41.5"],
+["darunter Briefwähler","4729","20.6","1.4","3802","19.2"],
+["Ungültige Stimmen","341","1.5","-0.8","451","2.3"],
+["Gültige Stimmen","22646","98.5","0.8","19385","97.7"],
+["DKP","55","0.2","-0.1","66","0.3"]];
+
+var data4 =["Potsdam",
+["Wahlberechtigte","135094","X","X","128782","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","86309","63.9","14.3","63897","49.6"],
+["darunter Briefwähler","23722","27.5","5.0","14353","22.5"],
+["Ungültige Stimmen","759","0.9","-0.6","961","1.5"],
+["Gültige Stimmen","85550","99.1","0.6","62936","98.5"],
+["DKP","218","0.3","-0.1","232","0.4"]];
+
+var data5 =["Barnim",
+["Wahlberechtigte","151567","X","X","148233","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","88862","58.6","13.5","66884","45.1"],
+["darunter Briefwähler","20963","23.6","4.1","13021","19.5"],
+["Ungültige Stimmen","1437","1.6","-0.7","1571","2.3"],
+["Gültige Stimmen","87425","98.4","0.7","65313","97.7"],
+["DKP","267","0.3","-0.1","256","0.4"]];
+
+var data6 =["Dahme-Spreewald",
+["Wahlberechtigte","136834","X","X","134573","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","86486","63.2","12.0","68953","51.2"],
+["darunter Briefwähler","18614","21.5","4.4","11773","17.1"],
+["Ungültige Stimmen","1673","1.9","-1.1","2069","3.0"],
+["Gültige Stimmen","84813","98.1","1.1","66884","97.0"],
+["DKP","205","0.2","-0.1","255","0.4"]];
+
+var data7 =["Elbe-Elster",
+["Wahlberechtigte","86349","X","X","91556","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","51947","60.2","9.4","46495","50.8"],
+["darunter Briefwähler","8464","16.3","3.8","5807","12.5"],
+["Ungültige Stimmen","1209","2.3","-1.9","1957","4.2"],
+["Gültige Stimmen","50738","97.7","1.9","44538","95.8"],
+["DKP","82","0.2","-0.1","95","0.2"]];
+
+var data8 =["Havelland",
+["Wahlberechtigte","131241","X","X","127809","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","78141","59.5","13.5","58800","46.0"],
+["darunter Briefwähler","17033","21.8","4.5","10177","17.3"],
+["Ungültige Stimmen","1104","1.4","-0.9","1333","2.3"],
+["Gültige Stimmen","77037","98.6","0.9","57467","97.7"],
+["DKP","70","0.1","0.0","76","0.1"]];
+
+var data9 =["Märkisch-Oderland",
+["Wahlberechtigte","159887","X","X","158572","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","95156","59.5","13.9","72311","45.6"],
+["darunter Briefwähler","21287","22.4","4.3","13032","18.0"],
+["Ungültige Stimmen","1661","1.7","-1.1","2075","2.9"],
+["Gültige Stimmen","93495","98.3","1.1","70236","97.1"],
+["DKP","266","0.3","-0.1","264","0.4"]];
+
+var data10 =["Oberhavel",
+["Wahlberechtigte","172531","X","X","169019","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","105515","61.2","14.0","79699","47.2"],
+["darunter Briefwähler","23125","21.9","2.9","15127","19.0"],
+["Ungültige Stimmen","1620","1.5","-0.7","1768","2.2"],
+["Gültige Stimmen","103895","98.5","0.7","77931","97.8"],
+["DKP","142","0.1","0.0","135","0.2"]];
+
+var data11 =["Oberspreewald-Lausitz",
+["Wahlberechtigte","92584","X","X","98281","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","52561","56.8","10.5","45553","46.3"],
+["darunter Briefwähler","10118","19.3","3.7","7068","15.5"],
+["Ungültige Stimmen","1173","2.2","-1.2","1546","3.4"],
+["Gültige Stimmen","51388","97.8","1.2","44007","96.6"],
+["DKP","54","0.1","0.0","58","0.1"]];
+
+var data12 =["Oder-Spree",
+["Wahlberechtigte","148370","X","X","150759","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","87732","59.1","10.0","74071","49.1"],
+["darunter Briefwähler","18355","20.9","3.6","12846","17.3"],
+["Ungültige Stimmen","1604","1.8","-1.0","2114","2.9"],
+["Gültige Stimmen","86128","98.2","1.0","71957","97.1"],
+["DKP","183","0.2","0.0","182","0.3"]];
+
+var data13 =["Ostprignitz-Ruppin",
+["Wahlberechtigte","82698","X","X","84740","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","44213","53.5","11.3","35720","42.2"],
+["darunter Briefwähler","8326","18.8","2.8","5736","16.1"],
+["Ungültige Stimmen","799","1.8","-1.1","1036","2.9"],
+["Gültige Stimmen","43414","98.2","1.1","34684","97.1"],
+["DKP","53","0.1","-0.1","62","0.2"]];
+
+var data14 =["Potsdam-Mittelmark",
+["Wahlberechtigte","172791","X","X","168741","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","113786","65.9","12.8","89615","53.1"],
+["darunter Briefwähler","26592","23.4","5.3","16198","18.1"],
+["Ungültige Stimmen","1823","1.6","-1.2","2518","2.8"],
+["Gültige Stimmen","111963","98.4","1.2","87097","97.2"],
+["DKP","203","0.2","0.0","186","0.2"]];
+
+var data15 =["Prignitz",
+["Wahlberechtigte","64169","X","X","67372","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","35810","55.8","13.4","28594","42.4"],
+["darunter Briefwähler","6700","18.7","4.4","4092","14.3"],
+["Ungültige Stimmen","660","1.8","-0.8","754","2.6"],
+["Gültige Stimmen","35150","98.2","0.8","27840","97.4"],
+["DKP","44","0.1","0.0","40","0.1"]];
+
+var data16 =["Spree-Neiße",
+["Wahlberechtigte","96723","X","X","102604","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","59374","61.4","12.3","50375","49.1"],
+["darunter Briefwähler","12424","20.9","3.6","8733","17.3"],
+["Ungültige Stimmen","1262","2.1","-1.5","1832","3.6"],
+["Gültige Stimmen","58112","97.9","1.5","48543","96.4"],
+["DKP","124","0.2","0.0","98","0.2"]];
+
+var data17 =["Teltow-Fläming",
+["Wahlberechtigte","135562","X","X","133945","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","81356","60.0","15.6","59484","44.4"],
+["darunter Briefwähler","17823","21.9","2.6","11461","19.3"],
+["Ungültige Stimmen","1485","1.8","-1.0","1679","2.8"],
+["Gültige Stimmen","79871","98.2","1.0","57805","97.2"],
+["DKP","134","0.2","-0.1","143","0.2"]];
+
+var data18 =["Uckermark",
+["Wahlberechtigte","99860","X","X","103892","X"],
+["Wähler / Wahlbeteiligung<sup>1</sup>","54438","54.5","10.8","45439","43.7"],
+["darunter Briefwähler","11429","21.0","4.3","7598","16.7"],
+["Ungültige Stimmen","1092","2.0","-1.3","1518","3.3"],
+["Gültige Stimmen","53346","98.0","1.3","43921","96.7"],
+["DKP","99","0.2","-0.1","110","0.3"]];
+
+var datasets = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18];
+var kreise = [];
+var zahlen2014abs = [];
+var zahlen2019abs = [];
+var deltaAbs = []
+var zahlen2014rel = [];
+var zahlen2019rel = [];
+var deltaRel = []
+
+for (let i=0; i<datasets.length; i++) {
+    kreise.push(datasets[i][0]);
+    zahlen2014abs.push(datasets[i][6][4]);
+    zahlen2019abs.push(datasets[i][6][1]);
+    deltaAbs.push(zahlen2019abs[i] - zahlen2014abs[i]);
+    zahlen2014rel.push(datasets[i][6][4] / datasets[i][5][4] * 100);
+    zahlen2019rel.push(datasets[i][6][1] / datasets[i][5][1] * 100);
+    deltaRel.push(zahlen2019rel[i] - zahlen2014rel[i]);
+}
+
+// Abs2014, Abs2019, AbsDelta, Rel2014, Rel2019, RelDelta
+var gesamtData = {
+    abs14: data0[6][4],
+    abs19: data0[6][1],
+    absDelta: data0[6][1] - data0[6][4],
+    rel14: data0[6][4] / data0[5][4] * 100,
+    rel19: data0[6][1] / data0[5][1] * 100,
+    relDelta: (data0[6][1] / data0[5][1] * 100) - (data0[6][4] / data0[5][4] * 100)
+}
